a3e29b5467dfb7666e237b05282a0afa609fc3b2
Mathew Duhon <mduhon@127x10.csc.calpoly.edu>
2010-11-15 15:58:48 -0800

    worked on it some more, having issues with reading

--- assign4/mytar.c
+++ assign4/mytar.c
@@ -14,6 +14,7 @@ typedef struct {
     char uid[8];
     char gid[8];
     char size[12];
+    long size2;
     char mtime[12];
     char chksum [8];
     char typeflag;
@@ -27,48 +28,71 @@ typedef struct {
     char prefix [155];
 } header;
 
-header *readHeader();
+header *readHeader(int inFile);
+int copyInfo(int size, int from, int to);
 
-int main () {
+
+int main (int argc, char * argv[]) {
 header *head;
-int file;
-
-    head = readHeader();
+int outFile, inFile;
+    if(argc != 2) {
+        perror("Usage");
+        exit(1);
+    }
+    if((inFile = open(argv[1], O_WRONLY, S_IRUSR | S_IWUSR)) < 0) {
+        perror("bad inFile");
+        exit(1);
+    }
+    head = readHeader( inFile);
     head->name[strlen(head->name)] = '1';
-    if(!(file = open(head->name, O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO))) {
+    if((outFile = open(head->name, O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG | S_IRWXO)) < 0) {
         perror("open");
         exit(3);
     }
 
-    write(file, head->size, strlen(head->size));
+    fprintf(stdout,"the size is %ld\n", head->size2);
+    if((write(outFile, &head->size2, sizeof(long)) < 0)) {
+            perror("write");
+            exit(3);
+    }
+    write(STDOUT_FILENO, &head->size2, sizeof(long int));
 
     return 0;
 }
-header *readHeader() {
+header *readHeader(int inFile) {
     header * head;
     head = malloc(sizeof(*head));
     if(!head) {
         perror("readHeader");
         exit(3);
     }
-    read(STDIN_FILENO, head->name, sizeof(head->name));
-    read(STDIN_FILENO, head->mode, sizeof(head->mode));
-    read(STDIN_FILENO, head->uid, sizeof(head->uid));
-    read(STDIN_FILENO, head->size, sizeof(head->size));
-    read(STDIN_FILENO, head->mtime, sizeof(head->mtime));
-    read(STDIN_FILENO, head->chksum, sizeof(head->chksum));
-    read(STDIN_FILENO, &head->typeflag, sizeof(head->typeflag));
-    read(STDIN_FILENO, head->linkname, sizeof(head->linkname));
-    read(STDIN_FILENO, head->magic, sizeof(head->magic));
-    read(STDIN_FILENO, head->version, sizeof(head->version));
-    read(STDIN_FILENO, head->uname, sizeof(head->uname));
-    read(STDIN_FILENO, head->gname, sizeof(head->gname));
-    read(STDIN_FILENO, head->devmajor, sizeof(head->devmajor));
-    read(STDIN_FILENO, head->devminor, sizeof(head->devminor));
-    read(STDIN_FILENO, head->prefix, sizeof(head->prefix));
+    read(inFile, head->name, sizeof(head->name));
+    read(inFile, head->mode, sizeof(head->mode));
+    read(inFile, head->uid, sizeof(head->uid));
+    read(inFile, head->size, sizeof(head->size));
+    read(inFile, head->mtime, sizeof(head->mtime));
+    read(inFile, head->chksum, sizeof(head->chksum));
+    read(inFile, &head->typeflag, sizeof(head->typeflag));
+    read(inFile, head->linkname, sizeof(head->linkname));
+    read(inFile, head->magic, sizeof(head->magic));
+    read(inFile, head->version, sizeof(head->version));
+    read(inFile, head->uname, sizeof(head->uname));
+    read(inFile, head->gname, sizeof(head->gname));
+    read(inFile, head->devmajor, sizeof(head->devmajor));
+    read(inFile, head->devminor, sizeof(head->devminor));
+    read(inFile, head->prefix, sizeof(head->prefix));
+    head->size2 = strtol(head->size,NULL, 8);
 /*    write(STDOUT_FILENO, head->name, strlen(head->name));
     write(STDOUT_FILENO, head->mode, strlen(head->mode));
     write(STDOUT_FILENO, head->uid, strlen(head->uid));*/
 
     return head;
 }
+
+
+
+
+int copyInfo(int size, int from, int to) {
+
+    return 0;
+} 
