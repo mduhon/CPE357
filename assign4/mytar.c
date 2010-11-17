945031e463262db4465ee553801e2e9602f7013a
mduhon <mdduhon@hotmail.com>
2010-11-17 11:45:36 -0800

    almost got extraction done

--- assign4/mytar.c
+++ assign4/mytar.c
@@ -1,5 +1,5 @@
 #include <string.h>
-
+#include <dirent.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,8 +13,8 @@ typedef struct {
     char mode[8];
     char uid[8];
     char gid[8];
-    char size[12];
-    long size2;
+    char pad[12];
+    long size;
     char mtime[12];
     char chksum [8];
     char typeflag;
@@ -34,34 +34,66 @@ int copyInfo(int size, int from, int to);
 
 int main (int argc, char * argv[]) {
 header *head;
+DIR * dircurr;
+struct dirent *curr_dir;
+struct stat curr_st;
 int outFile, inFile;
-    if(argc != 2) {
+    if(argc < 3) {
         perror("Usage");
         exit(1);
     }
-    if((inFile = open(argv[1], O_WRONLY, S_IRUSR | S_IWUSR)) < 0) {
+    if((inFile = open(argv[2], O_RDONLY, S_IRUSR | S_IWUSR)) < 0) {
         perror("bad inFile");
         exit(1);
     }
-    head = readHeader( inFile);
-    head->name[strlen(head->name)] = '1';
-    if((outFile = open(head->name, O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG | S_IRWXO)) < 0) {
+
+    dircurr = opendir(".");
+    curr_dir = readdir(dircurr);
+    while((head = readHeader(inFile))) {
+        if(!head->size)
+            continue;
+        head->name[strlen(head->name)] = '5';
+        if((outFile = open(head->name, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG | S_IRWXO)) < 0) {
+            perror("open");
+            exit(3);
+        }
+
+
+        copyInfo(((head->size/512)+1), inFile, outFile);
+        close(outFile);
+        free(head);
+    }
+/*    head = readHeader(inFile);
+    head->name[strlen(head->name)] = '5';
+    if((outFile = open(head->name, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG | S_IRWXO)) < 0) {
         perror("open");
         exit(3);
     }
 
-    fprintf(stdout,"the size is %ld\n", head->size2);
-    if((write(outFile, &head->size2, sizeof(long)) < 0)) {
-            perror("write");
-            exit(3);
+
+    copyInfo(((head->size/512)+1), inFile, outFile);
+    close(outFile);
+    free(head);
+
+    head = readHeader(inFile);
+    head->name[strlen(head->name)] = '5';
+    if((outFile = open(head->name, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG | S_IRWXO)) < 0) {
+        perror("open");
+        exit(3);
     }
-    write(STDOUT_FILENO, &head->size2, sizeof(long int));
+
+
+    copyInfo(((head->size/512)+1), inFile, outFile);
+
+    free(head);*/
+    close(inFile);
+    close(outFile);
 
     return 0;
 }
 header *readHeader(int inFile) {
     header * head;
-    head = malloc(sizeof(*head));
+    head = malloc(sizeof(header));
     if(!head) {
         perror("readHeader");
         exit(3);
@@ -69,7 +101,9 @@ header *readHeader(int inFile) {
     read(inFile, head->name, sizeof(head->name));
     read(inFile, head->mode, sizeof(head->mode));
     read(inFile, head->uid, sizeof(head->uid));
-    read(inFile, head->size, sizeof(head->size));
+    read(inFile, head->gid, sizeof(head->gid));
+    read(inFile, head->pad, sizeof(head->pad));
+    head->size = strtol(head->pad,NULL, 8);
     read(inFile, head->mtime, sizeof(head->mtime));
     read(inFile, head->chksum, sizeof(head->chksum));
     read(inFile, &head->typeflag, sizeof(head->typeflag));
@@ -81,10 +115,7 @@ header *readHeader(int inFile) {
     read(inFile, head->devmajor, sizeof(head->devmajor));
     read(inFile, head->devminor, sizeof(head->devminor));
     read(inFile, head->prefix, sizeof(head->prefix));
-    head->size2 = strtol(head->size,NULL, 8);
-/*    write(STDOUT_FILENO, head->name, strlen(head->name));
-    write(STDOUT_FILENO, head->mode, strlen(head->mode));
-    write(STDOUT_FILENO, head->uid, strlen(head->uid));*/
+    read(inFile, head->pad, sizeof(head->pad));
 
     return head;
 }
@@ -93,6 +124,18 @@ header *readHeader(int inFile) {
 
 
 int copyInfo(int size, int from, int to) {
+    int x, y;
+    unsigned char block[512];
 
+    for(x=0; x<size; x++) {
+        y = 0;
+        read(from, block, sizeof(block));
+        while(y<512) {
+            if(block[y] > 0) {
+                dprintf(to,"%c",block[y]);
+            }
+            y++;
+        }
+    }
     return 0;
 } 
